8488e3dbb8f6ade02983cf53cba4339d9d057884
Raphael Fichtinger <153192560+RaphaelFichtinger@users.noreply.github.com>
2024-03-22 14:01:34 +0100

    added dialog component from MD

--- ringoffire/src/app/game/game.component.ts
+++ ringoffire/src/app/game/game.component.ts
@@ -4,12 +4,15 @@ import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
 
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule ],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, ],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -19,7 +22,7 @@ export class GameComponent {
   game: Game = new Game;
 
 
-constructor() { }
+constructor(public dialog: MatDialog) { }
 
 ngOnInit(): void {
   this.newGame();
@@ -53,11 +56,13 @@ takeCard() {
       }, 1000);
 }
 }
+}
+openDialog(): void {
+  const dialogRef = this.dialog.open(DialogAddPlayerComponent,);
 
-
-
-
-
-
+  dialogRef.afterClosed().subscribe(result => {
+    console.log('The dialog was closed');
+  });
 }
 }
+
