f1ba65acddbd728926f88648b7f8bacd57ef3bd2
Raphael Fichtinger <153192560+RaphaelFichtinger@users.noreply.github.com>
2024-03-22 13:34:40 +0100

    added MD and btn

--- ringoffire/src/app/app.component.ts
+++ ringoffire/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
